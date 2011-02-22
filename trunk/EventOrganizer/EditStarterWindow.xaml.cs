a8e3713cc210190e09b00dbb2d4834871270e776
christoph.pangerl <christoph.pangerl@32e42c9e-3dc0-67fc-85d9-4b513d7c88a5>
2011-02-22 19:19:53 +0000

    Initial check in: - basic functionality to show all starter on the main-screen - functionality to create/edit a starter

---
+++ trunk/EventOrganizer/EditStarterWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EventOrganizer
+{
+    public partial class EditStarterWindow : Window
+    {
+        public Starter _starter;
+
+        public EditStarterWindow(Starter starter)
+        {
+            InitializeComponent();
+
+            _starter = starter;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _firstNameTextBox.Text = _starter.FirstName;
+            _lastNameTextBox.Text = _starter.LastName;
+            _clubTextBox.Text = _starter.Club;
+            _ponyTextBox.Text = _starter.Pony;
+            _commentTextBox.Text = _starter.Comment;
+
+            _birthdateDatePicker.SelectedDate = _starter.Birthdate;
+
+            // load competitions
+            using (EODbEntities db = new EODbEntities())
+            {
+                var competitions = from c in db.Competition
+                                   select new CheckedListItem<Competition>() { Item = c, Text = c.Caption };
+
+                List<CheckedListItem<Competition>> list = competitions.ToList();
+                foreach (var item in _starter.StarterCompetition)
+                {
+                    var s = list.Single(c => c.Item.Id == item.CompetitionId);
+                    s.IsChecked = true;
+                }
+
+                _competitionListBox.DataContext = list;
+            }
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            using (EODbEntities db = new EODbEntities())
+            {
+                db.Attach(_starter);
+
+                _starter.FirstName = _firstNameTextBox.Text;
+                _starter.LastName = _lastNameTextBox.Text;
+                _starter.Club = _clubTextBox.Text;
+                _starter.Pony = _ponyTextBox.Text;
+                _starter.Comment = _commentTextBox.Text;
+
+                _starter.Birthdate = _birthdateDatePicker.SelectedDate;
+
+                foreach (var item in (List<CheckedListItem<Competition>>)_competitionListBox.DataContext)
+                {
+                    bool itemExists = _starter.StarterCompetition.Any(c => c.CompetitionId == item.Item.Id);
+
+                    if (item.IsChecked && !itemExists)
+                    {
+                        // if the item is not checked in the database
+                        // and the user checked it now
+                        // create a new item and add it to startercompetition collection
+
+                        StarterCompetition sc = new StarterCompetition
+                                                    {
+                                                        CompetitionId = item.Item.Id,
+                                                        StarterId = _starter.Id
+                                                    };
+
+                        _starter.StarterCompetition.Add(sc);
+                    }
+                    else if (!item.IsChecked && itemExists)
+                    {
+                        // if the item is checked in the database
+                        // and the user unchecked it now
+                        // delete the item and remove it from the startercompetition collection
+
+                        var itemToDelete = _starter.StarterCompetition.Single(c => c.CompetitionId == item.Item.Id);
+                        db.DeleteObject(itemToDelete);
+                        _starter.StarterCompetition.Remove(itemToDelete);
+                    }
+                    else
+                    {
+                        // if the item state has not changed
+                        // do nothing
+                    }
+                }
+
+                db.SaveChanges();
+            }
+
+            this.DialogResult = true;
+            this.Close();
+        }
+    }
+}
---
+++ trunk/EventOrganizer/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventOrganizer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (EODbEntities db = new EODbEntities())
+            {
+                var query = from c in db.Starter.Include("StarterCompetition")
+                            orderby c.Id
+                            select c;
+
+                starterDataGrid.DataContext = query;
+            }
+        }
+
+        private void CloseMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AddStarterMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            EditStarterWindow ed = new EditStarterWindow(new Starter());
+            ed.ShowDialog();
+        }
+
+        private void EditStarterButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (starterDataGrid.SelectedItem is Starter)
+            {
+                EditStarterWindow ed = new EditStarterWindow((Starter)starterDataGrid.SelectedItem);
+                ed.ShowDialog();
+            }
+        }
+
+        private void starterDataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            // search for the row on which the user performed the doubleclick
+            DependencyObject dp = (DependencyObject)e.OriginalSource;
+            while (dp != null && !(dp is DataGridRow))
+            {
+                dp = VisualTreeHelper.GetParent(dp);
+            }
+
+            // if the row is not found, the user did not perform a doublclick on a row
+            if (dp is DataGridRow)
+            {
+                Starter starter = ((DataGridRow)dp).DataContext as Starter;
+                if (starter != null)
+                {
+                    EditStarterWindow ed = new EditStarterWindow(starter);
+                    ed.ShowDialog();
+                }
+            }
+        }
+    }
+}
