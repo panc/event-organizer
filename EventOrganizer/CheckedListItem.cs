4acdc79306c1f86de087f5bf1d03fc49a3344c3b
christoph.pangerl <christoph.pangerl@32e42c9e-3dc0-67fc-85d9-4b513d7c88a5>
2011-02-22 19:19:53 +0000

    Initial check in: - basic functionality to show all starter on the main-screen - functionality to create/edit a starter

---
+++ EventOrganizer/CheckedListItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventOrganizer
+{
+    public class CheckedListItem<T> where T : class
+    {
+        public string Text
+        {
+            get;
+            set;
+        }
+
+        public T Item
+        {
+            get;
+            set;
+        }
+
+        public bool IsChecked
+        {
+            get;
+            set;
+        }
+    }
+}
